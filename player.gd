1008c3a34f0bc08dba22f77a168aa1ec3e1d3d05
Michael Pitts <michaeljeffreypitts@gmail.com>
2024-08-27 11:46:30 -0700

    Added collisions and jumping, the creeps can now be squashed.

--- player.gd
+++ player.gd
@@ -4,6 +4,9 @@ extends CharacterBody3D
 
 @export var fall_acceleration = 75
 
+@export var jump_impulse = 20
+@export var bounce_impulse = 16
+
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
@@ -29,6 +32,26 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 		
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
+		
 	velocity = target_velocity
 	move_and_slide()
+	
+	for index in range(get_slide_collision_count()):
+		var collision = get_slide_collision(index)
+
+		if collision.get_collider() == null:
+			continue
+
+		if collision.get_collider().is_in_group("mob"):
+			
+			var mob = collision.get_collider()
+
+			if Vector3.UP.dot(collision.get_normal()) > 0.01:
+				
+				mob.squash()
+				target_velocity.y = bounce_impulse
+
+				break
 	
